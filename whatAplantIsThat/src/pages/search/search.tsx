934bbbbdabe5b34998e3c2be8dd2e09bf73dc074
Angelo <angelo.pjsol@gmail.com>
2020-12-04 13:59:53 -0300

    imageBackground - Error

--- whatAplantIsThat/src/pages/search/search.tsx
+++ whatAplantIsThat/src/pages/search/search.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { View, Text, ImageBackground } from 'react-native';
-import {Title, styles} from './style';
+import { View, Text, Image, ImageBackground , Dimensions} from 'react-native';
+import {Title, BackgroundContainer, FavoriteContainer, StarIcon} from './style';
 
-const background = '../../assets/images/searchScreen-bg.png';
+
+import starIcon from '../../assets/images/emptyStar.png';
+
+const { width, height } = Dimensions.get('window');
 
 function SearchPage() {
     return(
-        <ImageBackground style={styles.page} imageStyle={styles.bg} source={require(background)}>
-            <View>
-                <Text>4</Text>
-            </View>
-            <View>
-                <Title>What a Plant is That?</Title>
-                <Text>COMPONENTE - BARRA DE PESQUISA</Text>
-                <Text>COMPONENTE - PROCURA POR IMAGEM</Text>
-            </View>
-        </ImageBackground>
+        <View style={{flex:1, backgroundColor: 'blue'}}>
+            <BackgroundContainer />
+                {/* <FavoriteContainer>
+                    <StarIcon source={starIcon}/>
+                </FavoriteContainer> */}
+                {/* <View>
+                    <Title>What a Plant is That?</Title>
+                    <Text>COMPONENTE - BARRA DE PESQUISA</Text>
+                    <Text>COMPONENTE - PROCURA POR IMAGEM</Text>
+                </View> */}
+            {/* </BackgroundContainer> */}
+        </View>
     )
 };
 
--- whatAplantIsThat/src/pages/search/style.ts
+++ whatAplantIsThat/src/pages/search/style.ts
@@ -1,5 +1,9 @@
-import { StyleSheet, Dimensions } from 'react-native'
+import {Dimensions, ImageRequireSource} from 'react-native'
 import styled from 'styled-components/native';
+
+const { width, height } = Dimensions.get('window');
+
+import Background from '../../assets/images/searchScreen.png';
 
 export const Title = styled.Text`
     max-width: 200px;
@@ -8,21 +12,24 @@ export const Title = styled.Text`
     margin: 0 auto;
     font-weight: bold;
     color: #F6841B;
+`;
+
+export const BackgroundContainer = styled.Image.attrs({
+    imageStyle: {resizeMode:'stretch'}, source: Background
+})`
+    /* justify-content: center; */
+    background-color: blue;
+    width: 500px;
+    height: 300px;
+    flex: 1;
+`;
+
+export const FavoriteContainer = styled.View`
+    justify-content: center;
+    background-color: rgba(0, 0, 0, 0.69);
+    border: 1px solid #ABAC64;
+`;
+
+export const StarIcon = styled.Image`
+    width: 35px;
 `
-
-const { width, height } = Dimensions.get('window');
-export const styles = StyleSheet.create({
-    mainPage: {
-        justifyContent: 'center'
-    },
-
-    page: {
-        width: width,
-        height: height,
-        flex: 1
-    },
-
-    bg: {
-        resizeMode: 'stretch'
-    }
-})
