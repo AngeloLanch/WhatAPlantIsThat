180f172bdbda672462fb5098f9ac89bf1dc61c45
Angelo <angelo.pjsol@gmail.com>
2020-12-28 04:06:21 -0300

    imagePicker in progress

--- whatAplantIsThat/src/components/searchBar/index.tsx
+++ whatAplantIsThat/src/components/searchBar/index.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker/src';
+
 import { SearchBarContainer, TextContainer, TextArea, 
     TouchableIcon, IconsContainer, CameraImage, SearchImage } 
     from './style';
 
 function SearchBar() {
+    const [photo, setPhoto] = useState<String>('');
+
+    function HandleLaunchImage() {
+        launchImageLibrary({mediaType: 'photo'}, data => {
+
+            if(data.didCancel) {
+                return;
+            }
+
+            if(data.errorCode) {
+                return;
+            }
+
+            if(data.uri)
+            setPhoto(data.uri);
+        });
+    };
+
+
     return (
         <SearchBarContainer>
             <TextContainer>
                 <TextArea></TextArea>
             </TextContainer>
             <IconsContainer>
-                <TouchableIcon>
+                <TouchableIcon onPress={() => HandleLaunchImage()}>
                     <CameraImage />
                 </TouchableIcon>
                 <TouchableIcon>
