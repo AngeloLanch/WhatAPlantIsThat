42f1c3b1c64756a35e0b66f0dd6d0a8ecb030693
Angelo <angelo.pjsol@gmail.com>
2021-01-01 03:08:33 -0300

    searchBar component - refactoring

--- whatAplantIsThat/src/components/searchBar/index.tsx
+++ whatAplantIsThat/src/components/searchBar/index.tsx
@@ -1,57 +1,13 @@
-import React, {useState} from 'react';
-import {ActionSheet, Root} from 'native-base';
+import React from 'react';
+import {Root} from 'native-base';
 import {SearchBarContainer, TextContainer, TextArea, 
         TouchableIcon, IconsContainer, CameraImage, SearchImage} 
     from './style';
-import ImagePicker from 'react-native-image-crop-picker';
-import {ImageDataType} from '../../types/interface';
+import {useImagePicker} from './logic';
+
 
 function SearchBar() {
-    const [photo, setPhoto] = useState<ImageDataType>();
-
-    const HandleImagePicker = () => {
-        
-        const BUTTONS = [
-            {text: 'Take a shot'}, 
-            {text: "Select from Gallery"}, 
-            {text: 'Cancel'}
-        ];
-        
-        ActionSheet.show(
-            {
-              options: BUTTONS,
-              cancelButtonIndex: 2,
-              title: "Where is your little plant image?"
-            },
-            buttonIndex => {
-                if(buttonIndex == 0) {
-                    ImagePicker.openCamera({
-                        width: 300,
-                        height: 400,
-                        cropping: true
-                    })
-                    .then(image => {
-                        setPhoto(image)
-                    });
-                };
-
-                if(buttonIndex == 1) {
-                    ImagePicker.openPicker({
-                        width: 300,
-                        height: 400,
-                        cropping: true
-                    })
-                    .then(image => {
-                        setPhoto(image)
-                    });
-                };
-
-                if(buttonIndex !==0 && buttonIndex !==1) {
-                    return
-                };
-            }
-        )
-    };
+    const {HandleImagePicker, photo} = useImagePicker();
 
     return (
         <Root>
