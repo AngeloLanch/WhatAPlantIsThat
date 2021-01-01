0deba97bbc41bdddd4d8ba8970682db323f289bc
Angelo <angelo.pjsol@gmail.com>
2020-12-31 23:54:56 -0300

    pick photo - done

--- whatAplantIsThat/src/components/searchBar/index.tsx
+++ whatAplantIsThat/src/components/searchBar/index.tsx
@@ -1,38 +1,74 @@
-import React, { useState } from 'react';
-
-import { SearchBarContainer, TextContainer, TextArea, 
-    TouchableIcon, IconsContainer, CameraImage, SearchImage } 
+import React, {useState} from 'react';
+import {ActionSheet, Root} from 'native-base';
+import {SearchBarContainer, TextContainer, TextArea, 
+        TouchableIcon, IconsContainer, CameraImage, SearchImage} 
     from './style';
-
 import ImagePicker from 'react-native-image-crop-picker';
+import {ImageDataType} from '../../types/interface';
 
 function SearchBar() {
-    const [photo, setPhoto] = useState<String>('');
+    const [photo, setPhoto] = useState<ImageDataType>();
 
-    function HandleImagePicker() {
-        ImagePicker.openPicker({
-            width: 300,
-            height: 400,
-            cropping: true
-          }).then(image => {
-            console.log(image)
-          });
+    const HandleImagePicker = () => {
+        
+        const BUTTONS = [
+            {text: 'Take a shot'}, 
+            {text: "Select from Gallery"}, 
+            {text: 'Cancel'}
+        ];
+        
+        ActionSheet.show(
+            {
+              options: BUTTONS,
+              cancelButtonIndex: 2,
+              title: "Where is your little plant image?"
+            },
+            buttonIndex => {
+                if(buttonIndex == 0) {
+                    ImagePicker.openCamera({
+                        width: 300,
+                        height: 400,
+                        cropping: true
+                    })
+                    .then(image => {
+                        setPhoto(image)
+                    });
+                };
+
+                if(buttonIndex == 1) {
+                    ImagePicker.openPicker({
+                        width: 300,
+                        height: 400,
+                        cropping: true
+                    })
+                    .then(image => {
+                        setPhoto(image)
+                    });
+                };
+
+                if(buttonIndex !==0 && buttonIndex !==1) {
+                    return
+                };
+            }
+        )
     };
 
     return (
-        <SearchBarContainer>
-            <TextContainer>
-                <TextArea></TextArea>
-            </TextContainer>
-            <IconsContainer>
-                <TouchableIcon onPress={() => HandleImagePicker()}>
-                    <CameraImage />
-                </TouchableIcon>
-                <TouchableIcon>
-                    <SearchImage />
-                </TouchableIcon>
-            </IconsContainer>
-        </SearchBarContainer>
+        <Root>
+            <SearchBarContainer>
+                <TextContainer>
+                    <TextArea></TextArea>
+                </TextContainer>
+                <IconsContainer>
+                    <TouchableIcon onPress={() => HandleImagePicker()}>
+                        <CameraImage />
+                    </TouchableIcon>
+                    <TouchableIcon>
+                        <SearchImage />
+                    </TouchableIcon>
+                </IconsContainer>
+            </SearchBarContainer>
+        </Root>
     )
 }
 
---
+++ whatAplantIsThat/src/types/interface.ts
@@ -0,0 +1,8 @@
+export interface ImageDataType {
+    "cropRect"?: {"height": number, "width": number, "x": number, "y": number} | null,
+    'height': number,
+    'mime': string,
+    "path": string,
+    'size': number,
+    'width': number
+};
